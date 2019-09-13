2ff3aa263f88b12137ad58fd4a0b59f0b72107ef
ik311015 <ik311015@ohio.edu>
2019-09-13 10:36:13 -0400

    added controllers and changed config file connection string

---
+++ Controllers/ordersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ik311015_MIS4200.DAL;
+using ik311015_MIS4200.Models;
+
+namespace ik311015_MIS4200.Controllers
+{
+    public class ordersController : Controller
+    {
+        private MIS4200Context db = new MIS4200Context();
+
+        // GET: orders
+        public ActionResult Index()
+        {
+            var order = db.Order.Include(o => o.customer);
+            return View(order.ToList());
+        }
+
+        // GET: orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.customerId = new SelectList(db.Customer, "customerId", "lastName");
+            return View();
+        }
+
+        // POST: orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "orderId,customerId,orderDate")] order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Order.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.customerId = new SelectList(db.Customer, "customerId", "lastName", order.customerId);
+            return View(order);
+        }
+
+        // GET: orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.customerId = new SelectList(db.Customer, "customerId", "lastName", order.customerId);
+            return View(order);
+        }
+
+        // POST: orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "orderId,customerId,orderDate")] order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.customerId = new SelectList(db.Customer, "customerId", "lastName", order.customerId);
+            return View(order);
+        }
+
+        // GET: orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            order order = db.Order.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            order order = db.Order.Find(id);
+            db.Order.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
